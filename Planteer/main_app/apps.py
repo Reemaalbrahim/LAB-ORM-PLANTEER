0bfb5c4ff90f4dbb2473465ac7a5041ba6a03e1e
Reema <reemaalbrahim28@gmail.com>
2024-11-06 14:04:23 +0300

    Model are successfully created

--- Planteer/main_app/apps.py
+++ Planteer/main_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ContactAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'contact_app'
+    name = 'main_app'
--- Planteer/plants_app/models.py
+++ Planteer/plants_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Plants(models.Model): 
+     
+    class Category(models.TextChoices):
+       FLOWERS = 'Flowers', 'Flowers'
+       TREES = 'Trees', 'Trees'
+       FRUITS = 'Fruits', 'Fruits'
+       VEGETABLES = 'Vegetables', 'Vegetables'
+        
+    name= models.CharField(max_length=255)
+    about  = models.TextField()
+    used_for = models.TextField()
+    image = models.ImageField(upload_to="images/", default="images/default.png")
+    category = models.CharField(max_length=20, choices=Category.choices, default=Category.TREES)
+    is_edible = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+
+def __str__(self):
+    return self.name 
+     
+     
+    
+
